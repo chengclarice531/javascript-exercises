a942095e955a60671289fb62c2863473bf18956e
chengclarice531 <chengclarice531@gmail.com>
2025-06-20 15:55:36 +0100

    Sum the tripled vaules

--- loops_and_arrays/sumOfTripledEvens.js
+++ loops_and_arrays/sumOfTripledEvens.js
@@ -2,4 +2,5 @@ function sumOfTripledEvens(array) {
   return array
     .filter((num) => num % 2 === 0) // Filter even numbers
     .map((num) => num * 3) // Triple each even number
+    .reduce((sum, num) => sum + num, 0); // Sum the tripled values
 }
